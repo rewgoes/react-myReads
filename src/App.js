c51436f1a431b645ee46254a63279ea17874c745
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-05-09 23:09:40 -0300

    feat: move the UI content for each component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks.js'
 import SearchBooks from './SearchBooks.js'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks books={this.state.books} />
         )} />
         <Route path="/search" render={() => (
           <SearchBooks />
